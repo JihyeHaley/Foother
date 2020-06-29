2c580d694efc200178b98c5849031070d41391c9
dongkyu92 <dongkyu92@naver.com>
2020-06-29 20:41:42 +0900

    Search implements

--- 00_1_Footer_Haley_Creation/foother/maps/urls.py
+++ 00_1_Footer_Haley_Creation/foother/maps/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 app_name = 'maps'
-
 urlpatterns = [
     path('', views.index, name='index'),
+    path('review/', views.review, name='review'),
 ]
--- 00_1_Footer_Haley_Creation/foother/maps/views.py
+++ 00_1_Footer_Haley_Creation/foother/maps/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from decouple import config
+from .forms import ReviewForm
 
 # Create your views here.
 def index(request):
     pass
+
+def review(request):
+    # user = request.user
+    form = ReviewForm()
+    key = config('API_KEY')
+
+    context = {
+        'form' : form,
+        'key' : key,
+    }
+    return render(request, 'maps/_chuck_main_input.html', context)
+    
