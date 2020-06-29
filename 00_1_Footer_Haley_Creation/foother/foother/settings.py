45991803c7cfe28e288f5e980dbef80a9fceb7b5
dongkyu92 <dongkyu92@naver.com>
2020-06-29 15:26:59 +0900

    backup

--- 00_1_Footer_Haley_Creation/foother/foother/settings.py
+++ 00_1_Footer_Haley_Creation/foother/foother/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'accounts',
+    'roots',
+    'maps',
     'imagekit',
     'django_extensions',
     'bootstrap4',
@@ -122,6 +124,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'foother', 'static'),
+]
+
 AUTH_USER_MODEL = 'accounts.User' 
 
 MEDIA_URL = '/media/'
