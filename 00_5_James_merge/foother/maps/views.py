7984ccc99a7b33e6ee9dcf457724cf3d4be4b113
JihyeHaley <ohhojh@gmail.com>
2020-07-03 02:30:02 +0900

    succed like function

--- 00_5_James_merge/foother/maps/views.py
+++ 00_5_James_merge/foother/maps/views.py
@@ -3,6 +3,7 @@ from decouple import config
 from .forms import ReviewForm
 from .models import Review
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
 
 
 @login_required
@@ -25,3 +26,24 @@ def create(request):
         # 'key' : key,
     }
     return render(request, 'maps/review_create.html', context)
+
+
+@login_required
+def like(request, review_pk):
+    review = get_object_or_404(Review, pk=review_pk)
+    user = request.user
+
+    if review in user.like_reviews.all():
+        review.like_reviews.remove(review)
+        liked = False
+
+    else:
+        review.like_reviews.add(review)
+        liked= True
+
+    context = {
+        'msg' : 'like works',
+        'liked' : liked,
+    }
+
+    return JsonResponse(context)
